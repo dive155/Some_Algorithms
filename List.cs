0828f6b1ea4b3965be4dddbf926076b4e2094234
dive155 <divedimon@mail.ru>
2017-07-08 05:15:30 +0300

    Sortings and an implementation of a list

---
+++ List.cs
@@ -0,0 +1,172 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class List : MonoBehaviour {
+	public GUIText textUI;
+
+	public int[] elements;
+	public int insertionIndex;
+	public int insertionValue;
+	public int removalIndex;
+	public int searchValue;
+
+	public class Item {//an item for the list
+		public int index;
+		public int value; 
+		public Item next;
+
+		public void setValue(int val) {
+			value = val;
+		}
+
+		public void setIndex(int val) {
+			index = val;
+		}
+	}
+
+	public class TheList { //the list itself
+		private Item head;
+		public int length = 0;
+
+		public string PrintList(){ //printing the list
+			string OutString;
+			OutString = string.Format("Length of the list: {0}.\n", length);  //the header
+
+			Item current = head; //starting from the first element
+			while (current != null) {
+				OutString += string.Format("List[{0}]: {1}\n", current.index, current.value); //writing the value of the current element
+				current = current.next;
+			}
+			OutString += "\n";
+			return OutString;
+		}
+
+		public void addEnd(int val) { //adding an element to the end of the list
+			Item newElem = new Item (); //creating new element
+			newElem.setValue (val); //setting the value
+			newElem.next = null; //nothing follows the element
+			length++; //incrementing the length of the list
+
+			if (head == null) { //if it's the first element
+				newElem.setIndex(0); 
+				head = newElem; //saving it as the first element
+			} else { //if it's not the first element
+				int i = 1;
+				Item current = head; 
+				while (current.next != null) {//searching for the last element
+					current = current.next;
+					i++;
+				}
+				newElem.setIndex (i); //assigning the index
+				current.next = newElem; //attaching the new element to the last one
+			}
+		}
+
+		public void Insert (int ind, int val){
+			if (ind > length - 1) //making sure we're not out of range
+				return;
+
+			Item newElem = new Item (); //creating new element
+			newElem.setValue (val);
+
+
+			if (ind == 0) { //if inserting to the head of the list
+				newElem.next = head; //the next element
+				head = newElem; //we have a new head
+				FixIndexes ();
+				length++;
+				return;
+			}
+				
+			Item current = head;
+			for (int i = 0; i < ind-1; i++) {
+				current = current.next; //moving to the element ind-1
+			}
+
+			Item nextElem = current.next; //remembering the next element
+			current.next = newElem; //putting the new one in it's place
+			newElem.next = nextElem; //attaching the old next element to the newly inserted one
+			length++;
+			FixIndexes ();
+		}
+
+		public void Remove (int ind) {
+			if (ind > length - 1) //making sure we're not out of range
+				return;
+
+			Item current = head;
+			length--;
+
+			if (ind == 0) { //if removing the first element
+				head = head.next;
+				FixIndexes ();
+				return;
+			}
+
+			for (int i = 0; i < ind-1; i++) {
+				current = current.next; //moving to the element ind-1
+			}
+
+			Item nextElem = current.next;
+			nextElem = nextElem.next; //finding an element after the one we delete
+			current.next = nextElem; //fixing our chain
+
+			FixIndexes ();
+		}
+
+		public void FixIndexes() { //making sure indexes are ok
+			Item current = head; 
+			int j = 0;
+			while (current != null) { //just go through all the elements
+				current.setIndex (j); //and assign indexes to them
+				current = current.next;
+				j++;
+			}
+		}
+
+		public int SearchValue (int val) {  //searching by value
+			Item current = head; //starting from the first element
+			while (current != null) { //searching through elements
+				if (current.value == val) { //got eem!
+					return current.index;
+				}
+				current = current.next;
+			}
+			return -1; //aint got eem, error code
+		}
+
+	}
+	// Use this for initialization
+	void Start () {
+		TheList myList = new TheList ();
+		for (int i = 0; i < elements.Length; i++) {
+			myList.addEnd (elements [i]); //adding elements from the array into the list
+		}
+
+		textUI.text += "The source list:\n";
+		textUI.text += myList.PrintList();
+
+		myList.Insert (insertionIndex, insertionValue);
+		textUI.text += string.Format("Inserting the value {0} into the spot indexed {1}:\n", insertionValue, insertionIndex); //printing the message
+		textUI.text += myList.PrintList();
+
+		myList.Remove (removalIndex);
+		textUI.text += string.Format("Removing the item indexed {0}:\n", removalIndex);
+		textUI.text += myList.PrintList();
+
+		int searchInd = myList.SearchValue (searchValue);
+		if (searchInd == -1) {
+			textUI.text += string.Format ("An element with value {0} was not found.\n", searchValue);
+		} else {
+			textUI.text += string.Format ("An element with value {0} was found at index {1}.\n", searchValue, searchInd);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Sort1.cs
@@ -0,0 +1,165 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sort1 : MonoBehaviour {
+
+	public GUIText textUI;
+
+	public int[] ints;
+	private int[] ints1;
+	private int[] ints2;
+	private int[] ints3;
+	//private int[] pints = new int[10];
+
+	// Use this for initialization
+	void Start () {
+		ints1 = ints; //making a copy of the source array to be used in sorting
+		ints2 = ints;
+		ints3 = ints;
+
+		//InvokeRepeating("Iterate", 1.0f, 1.0f);
+		textUI.text += "The source array is: \n";
+		PrintArray (ints);
+
+		textUI.text += "Sorted via bubble sort: \n";
+		BubbleSort (ints1);
+		PrintArray (ints1);
+
+		textUI.text += "Sorted via merging sort: \n";
+		MergeSort (ints2);
+		PrintArray (ints2);
+
+		textUI.text += "Sorted via quick sort: \n";
+		QuickSort (ints3, 0, ints3.Length-1);
+		PrintArray (ints3);
+
+		textUI.text += "Fibonacci numbers: \n1 1 ";
+		Fibonacci (1, 1);
+		textUI.text += "\n";
+		textUI.text += string.Format ("numba one {0} numba two {1}", 1, 2);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	//one iteration of doing things
+	void Iterate() {
+		//textUI.text += "dope stuff \n";
+		PrintArray (ints);
+		BubbleSort (ints);
+	}
+
+	//printing an array
+	void PrintArray (int[] array)
+	{
+		for (int i = 0; i < array.Length; i++) {
+			textUI.text += array [i];
+			textUI.text += " ";
+		}
+		textUI.text += "\n";
+	}
+
+	//bubble sort
+	void BubbleSort (int[] array) {
+		int length = array.Length;
+		int j = length - 1;
+		do { 
+			for (int i = 0; i < length - 1; i++) {
+				if (array [i] > array [i + 1]) { //if the next one is smaller, then swap the two
+					Swap (array, i, i+1);
+				}
+			}
+			j--; //no need to check the last one again
+		} while (j > 2);
+	}
+
+	void Swap (int[] array, int el1, int el2) { //swap two elements
+		int k = array [el1];
+		array [el1] = array [el2];
+		array [el2] = k;
+	}
+		
+	void Merge (int[] L, int[] R, int[] A) { //merge two arrays in increasing order
+		int nl = L.Length; 
+		int nr = R.Length;
+		int i = 0;
+		int j = 0;
+		int k = 0;
+
+		while (i < nl && j < nr) { //while there are unpicked elements
+			if (L [i] <= R [j]) { //comparing smallest unpicked elements
+				A [k] = L [i]; //if the element in the left array is smaller, put it into A
+				k++; //next element in A
+				i++; //smallest unpicked element  in L
+			} else {
+				A [k] = R [j];
+				k++;
+				j++;
+			}
+		}
+		while (i < nl) { //if there are no elements left in R
+			A [k] = L [i];
+			i++;
+			k++;
+		}
+		while (j < nr) { //if there are no elements left in L
+			A [k] = R [j];
+			j++;
+			k++;
+		}
+	}
+
+	void MergeSort (int[] A) {
+		int n = A.Length;
+		if (n < 2) //an array of length 1 is always sorted
+			return;
+		int mid = n / 2; //middle of the array
+		int[] left = new int[mid]; //left half
+		int[] right = new int[n - mid]; //right half
+		for (int i = 0; i <= mid - 1; i++)
+			left [i] = A [i]; //saving the halves
+		for (int i = mid; i <= n - 1; i++)
+			right [i - mid] = A [i];
+		MergeSort (left); //sorting each half
+		MergeSort (right);
+		Merge (left, right, A); //merging them together
+	}
+
+	void QuickSort (int[] A, int start, int end) {
+		if (start < end) {
+			int pIndex = Partition (A, start, end); //dividing into two parts
+			QuickSort (A, start, pIndex - 1); //sorting each part
+			QuickSort (A, pIndex + 1, end);
+		}
+	}
+
+	int Partition (int[] A, int start, int end){
+		//choose an arbitrary element (pivot) (we pick the last element)
+		//then arrange the array so that all the elements < pivot
+		//are to the left of the pivot, and vice versa
+		int pivot = A [end]; //pick the last element as pivot
+		int pIndex = start; //starting comparing from the start
+		for (int i = start; i <= end - 1; i++) {
+			if (A [i] <= pivot) { //if an element is lesser than the pivot
+				Swap (A, i, pIndex); //it should be to the left of it
+				pIndex++; //keep organising
+			}
+		}
+		Swap (A, pIndex, end); //putting the last element into the place
+		return pIndex;
+	}
+
+	void Fibonacci(int a, int b) {
+		int c = a + b; //sum of previous two numbers
+		textUI.text += c;
+		textUI.text += " ";
+		if (c > 600) //end condition
+			return;
+		Fibonacci (b, c); //next number
+	}
+
+}
+	
